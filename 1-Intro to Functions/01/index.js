7e960b7f427f0e71351074c77a14256d19e7c801
5n0weirdo <snoweirdo@protonmail.com>
2021-09-24 19:03:45 +0200

    Intro to Functions

---
+++ 1-Intro to Functions/01/index.js
@@ -0,0 +1,13 @@
+// print greeting in a function
+function sayHello() {
+  console.log("Hello World");
+}
+sayHello();
+
+// loop 0-6 in a function
+function count() {
+  for (var i = 0; i <= 6; i++) {
+    console.log(i);
+  }
+}
+count();
