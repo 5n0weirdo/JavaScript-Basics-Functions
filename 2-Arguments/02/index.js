c99ec22bd6dc18e08a23eada4e47458026125941
5n0weirdo <snoweirdo@protonmail.com>
2021-09-26 07:03:44 +0200

    Arguments

---
+++ 2-Arguments/02/index.js
@@ -0,0 +1,17 @@
+function sayHello(name) {
+  console.log("Hello " + name);
+}
+sayHello("Web");
+
+// A Function that calculates an area:
+function area(length, width) {
+  console.log(length * width);
+}
+area(9,9); //81
+
+// A Function that greets 3 people:
+function greet(person1, person2, person3) {
+  console.log("Hi " + person1)
+  console.log("Hi " + person2)
+}
+greet("Web", "Design");
